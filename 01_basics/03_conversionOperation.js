17b58280419229001f0fad72f6ab234c46027607
Hemant Garg <99550853+Hemantgarg06@users.noreply.github.com>
2025-02-14 11:51:19 +0000

    Conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Hemant"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//"33" => 33 
+// "33abc" => NaN  but its type is showing number
+// true => 1
+//false => 0
+
+let isLoggenIn = "Hemant"
+
+let booleanIsLoggedIn = Boolean(isLoggenIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
