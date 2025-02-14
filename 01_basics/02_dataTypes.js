890a6dcc0e6460c68bcc2b6611af5272d57fd0bd
Hemant Garg <99550853+Hemantgarg06@users.noreply.github.com>
2025-02-14 11:14:29 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";  //treat all JS code as newer version
+
+// alert(3 + 3) //we are using nodejs, not browser
+
+let name = "Hemant"
+let age = 23
+let isLoggedIn = false
+
+// Data Types
+// number => 2^53
+// bigint
+// string = ""
+// boolen => true/
+// null => standalone value
+// undefined => abhi value assign nhi hui hai
+// symbol => unique
+
+console.log(typeof undefined);  //undefined aaega qki ye ek type hai js k andar
+console.log(typeof null);  //object
