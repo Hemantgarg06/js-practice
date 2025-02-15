c3cb04d245f997c3fa5bf0876d13aad0be87deb9
Hemant Garg <99550853+Hemantgarg06@users.noreply.github.com>
2025-02-15 11:15:39 +0000

    Memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -36,3 +36,27 @@ console.log(typeof id);
 console.log(typeof null);   //typeof null is object
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+// *******************************************************
+
+// Stack (Primitive), Heap (Non-Primitive)
+// Stack me copy milta hai aur Heap me reference milta hai
+let myYouTubeName = "HemantGargdotcom"
+
+let anotherName = myYouTubeName
+anotherName = "chaiaurcode"
+
+console.log(myYouTubeName);
+console.log(anotherName);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "Hemant@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
