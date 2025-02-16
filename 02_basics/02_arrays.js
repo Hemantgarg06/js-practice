e33841805d870c4a737a2cf54694ef1b8ec12161
Hemant Garg <99550853+Hemantgarg06@users.noreply.github.com>
2025-02-16 09:30:35 +0000

    some changes

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -16,14 +16,14 @@ const dc_heros = ["superman", "flash", "batman"]
 const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
 
 const real_another_array = another_array.flat(Infinity)
-console.log(real_another_array);
+// console.log(real_another_array);
 
 console.log(Array.isArray("Hemant"))
-console.log(Array.from("Hemant"))   //jo bhi value denge use ye array me convert kar dega
-console.log(Array.from({name: "Hemant"}))   //interesting coz it will give empty array [] qki aesa cases me hme bta hoga ki ye key value pair hai
+// console.log(Array.from("Hemant"))   //jo bhi value denge use ye array me convert kar dega
+// console.log(Array.from({name: "Hemant"}))   //interesting coz it will give empty array [] qki aesa cases me hme bta hoga ki ye key value pair hai
 
 let score1 = 100
 let score2 = 200
 let score3 = 300
 
-console.log(Array.of(score1, score2, score3));
+// console.log(Array.of(score1, score2, score3));
